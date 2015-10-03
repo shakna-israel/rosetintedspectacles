d727f63c228a8927e8d763fec75ee6acaf697b2d
James Milne <s4b3r6@gmail.com>
2015-10-03 23:22:11 +1000

    Update select_controls.js

--- js/select_controls.js
+++ js/select_controls.js
@@ -1,16 +1,21 @@
 function setFocus(element){
   element.focus();
+  return document.activeElement;
 }
 
 function nextText(){
   setFocus(document.activeElement.nextElementSibling);
+  return document.activeElement;
 }
 
 function lastText(){
   setFocus(document.activeElement.previousElementSibling);
+  return document.activeElement;
 }
 
-window.onload = function() {
+function main() {
   var ele = document.getElementById('readable').childNodes[1];
   setFocus(ele);
 }
+
+window.onload = main()
