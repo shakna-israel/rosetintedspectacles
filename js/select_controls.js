d3dc07ab60691af52c2bd47b322518a2ef16e7ed
Shakna Israel <s4b3r6@gmail.com>
2015-10-03 13:00:00 +0000

    nextText and lastText added.

--- js/select_controls.js
+++ js/select_controls.js
@@ -2,7 +2,17 @@ function setFocus(element){
   element.focus();
 }
 
+function nextText(element){
+  setFocus(element.nextElementSibling);
+  return element.nextElementSibling;
+}
+
+function lastText(element){
+  setFocus(element.previousElementSibling);
+  return element.previousElementSibling;
+}
+
 window.onload = function() {
-  var ele = document.getElementById('readable').childNodes[1]
-  setFocus(ele)
+  var ele = document.getElementById('readable').childNodes[1];
+  setFocus(ele);
 }
