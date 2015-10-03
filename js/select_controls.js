a9a120ab405d59f5928872443a620351181fa380
James Milne <s4b3r6@gmail.com>
2015-10-03 23:50:10 +1000

    Update select_controls.js

--- js/select_controls.js
+++ js/select_controls.js
@@ -4,18 +4,17 @@ function setFocus(element){
 }
 
 function nextText(){
-  setFocus(document.activeElement.nextSibling);
+  setFocus(document.activeElement.nextElementSibling);
   return document.activeElement;
 }
 
 function lastText(){
-  setFocus(document.activeElement.previousSibling);
+  setFocus(document.activeElement.previousElementSibling);
   return document.activeElement;
 }
 
 function checkKey(e) {
     var event = window.event ? window.event : e;
-    console.log(event.keyCode)
     
     if (e.keyCode == '39') {
       nextText()
