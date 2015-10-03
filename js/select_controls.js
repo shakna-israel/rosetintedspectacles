a84e54c9f1826d613d21034fb1ac337332f1c7eb
Shakna Israel <s4b3r6@gmail.com>
2015-10-03 12:34:18 +0000

    Different approach

--- js/select_controls.js
+++ js/select_controls.js
@@ -2,6 +2,6 @@ function setFocus(element){
   element.focus();
 }
 
-var ele = document.getElementbyName('body').children[0]
+var ele = document.getElementbyId('readable').children[0]
 
 window.onload = setFocus(ele)
